07371cc1849488459d5e90ad0c7967b6f63c5eba
DulceChaidez <a21490535@itmexicali.edu.mx>
2025-04-25 20:20:15 -0700

    prueba flask

---
+++ Src/app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from config import config
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return "<h1>Login<h1>"
+if __name__ == '__main__':
+    app.config.from_object(config['development'])
+    app.run()
